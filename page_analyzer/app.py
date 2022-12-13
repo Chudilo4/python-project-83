d44bed2b2f9d57dc4437e1badc4f6675a653f91c
chudilo4 <chipalinchik@gmail.com>
2022-12-13 21:20:04 +0500

    fix step three

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, get_flashed_messages
+from flask import Flask, render_template, request, redirect, flash, get_flashed_messages, url_for
 from dotenv import dotenv_values
 import psycopg2
 import os
@@ -16,7 +16,7 @@ app.secret_key = 'sdfsggao23ko3t89*U$t4nt4*H#rhfnsdnm,'
 
 
 @app.route('/')
-def hello_world():
+def index():
     messages = get_flashed_messages()
     return render_template(
         'home.html',
@@ -30,29 +30,38 @@ def urls_add():
     dt = datetime.datetime.now()
     form = request.form.to_dict()
     valid_url = validators.url(form['url'])
-    if valid_url:
+    # Если это url
+    if valid_url and len(form['url']) <= 255:
         cur = conn.cursor()
+        # Смотрим похожее имя в БД
         cur.execute('SELECT id FROM urls WHERE name=(%s);', (form['url'],))
         id_find = cur.fetchone()
         cur.close()
-        if id:
-            flash('Не верный URL')
-            return redirect(f'/')
-        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);",
-                    (form['url'], dt))
-        id_insert = cur.fetchone()
+        print(id_find)
+        # Если такая запись в БД уже есть
+        if id_find:
+            flash('Такой сайт уже есть')
+            return redirect(url_for('show_url', id=id_find[0]))
+        # Продолжаем если в базе записи нет
+        cur = conn.cursor()
+        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);", (form['url'], dt))
+        cur.execute('SELECT id FROM urls WHERE name=(%s);', (form['url'],))
+        id_find = cur.fetchone()
         cur.close()
         flash('Страница успешно добавлена')
-        return redirect(f'/urls/{id_insert[0]}')
-
+        return redirect(url_for('show_url', id=id_find[0]))
+    flash('Не верный URL')
+    return redirect(url_for('index'))
 
 @app.get('/urls/')
 def get_urls():
     cur = conn.cursor()
     cur.execute('SELECT urls.id, urls.name, url_checks.created_at '
-                'FROM urls LEFT JOIN url_checks '
-                'ON urls.id = url_checks.url_id;')
+                'FROM urls JOIN url_checks '
+                'ON urls.id = url_checks.url_id '
+                'ORDER BY urls.id;')
     site = cur.fetchall()
+    cur.close()
     return render_template('urls.html', site=site)
 
 
@@ -72,8 +81,15 @@ def show_url(id):
 def urls_id_checks_post(id):
     dt = datetime.datetime.now()
     cur = conn.cursor()
-    cur.execute('INSERT INTO url_checks (created_at, url_id ) VALUES (%s, %s);', (dt, id))
+    cur.execute('SELECT * FROM url_checks WHERE url_id = (%s);', (id,))
+    id_find = cur.fetchone()
+    if id_find:
+        cur.execute('UPDATE url_checks SET created_at = (%s) '
+                    'WHERE url_id = (%s);', (dt, id,))
+        cur.close()
+        flash('Страница успешно проверена')
+        return redirect(url_for('show_url', id=id))
+    cur.execute('INSERT INTO url_checks (url_id, created_at) VALUES ((%s), (%s));', (id, dt))
     cur.close()
     flash('Страница успешно проверена')
-    return redirect(f'/urls/{id}/', )
-
+    return redirect(url_for('show_url', id=id))
