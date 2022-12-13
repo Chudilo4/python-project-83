dbc66c04a1b30543ead948b3f74fae0b9b1f7ee9
chudilo4 <chipalinchik@gmail.com>
2022-12-13 15:52:50 +0500

    step three

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,6 +4,7 @@ import psycopg2
 import os
 import validators
 import datetime
+import requests
 
 
 # Connect to your postgres DB
@@ -30,19 +31,23 @@ def urls():
             cur = conn.cursor()
             dt = datetime.date.today()
             names = a['url']
-            cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);",
-                        (names, dt))
-            cur.execute("SELECT id FROM urls WHERE name=(%s);",
-                        (names,))
+            r = cur.execute('SELECT id FROM urls WHERE name=(%s), (names);')
+            if r:
+                cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);",
+                            (names, dt))
+                id = cur.fetchone()
+                cur.close()
+                return redirect(f'/urls/{id[0]}')
             id = cur.fetchone()
-            print(id)
             cur.close()
             return redirect(f'/urls/{id[0]}')
         return redirect('/')
     else:
         cur = conn.cursor()
-        cur.execute('SELECT * FROM urls;')
+        cur.execute('SELECT urls.id, urls.name, url_checks.created_at, url_checks.status_code '
+                    'FROM urls LEFT JOIN url_checks ON urls.id = url_checks.url_id;')
         site = cur.fetchall()
+        print(site)
         cur.close()
         return render_template('urls.html', site=site)
 
@@ -50,7 +55,30 @@ def urls():
 @app.route('/urls/<int:id>/', methods=['GET', 'POST'])
 def show_url(id):
     cur = conn.cursor()
-    cur.execute('SELECT * FROM urls WHERE id = (%s);', (id, ))
+    cur.execute('SELECT * FROM urls WHERE id=(%s);', (id,))
     site = cur.fetchone()
+    cur.execute('SELECT * FROM url_checks WHERE url_id=(%s);', (id,))
+    site2 = cur.fetchone()
+    print(site2)
+    print(site)
     cur.close()
-    return render_template('show_url.html', site=site)
+    return render_template('show_url.html', site=site, site2=site2)
+
+
+@app.post('/urls/<int:id>/checks')
+def urls_id_checks_post(id):
+    dt = datetime.date.today()
+    cur = conn.cursor()
+    a = cur.execute('SELECT * FROM url_checks WHERE url_id = (%s)', (id,))
+    print(bool(a))
+    print(a)
+    if a:
+        cur.execute('UPDATE url_checks '
+                    'SET created_at = (%s) WHERE url_id = (%s);', (dt, id))
+        cur.close()
+        return redirect(f'/urls/{id}/')
+    cur.execute("INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s);",
+                (id, dt))
+    cur.close()
+    return redirect(f'/urls/{id}/')
+
