12168a90019a584906e7f981ff24cdece3395513
chudilo4 <chipalinchik@gmail.com>
2022-12-14 17:27:59 +0500

    Переместил database.sql в корень

--- 1.py
+++
@@ -1,7 +0,0 @@
-from dotenv import dotenv_values
-import psycopg2
-import os
-
-conn = psycopg2.connect(os.getenv('DATABASE_URL'))
-conn.autocommit = True
-conn.cursor().execute(open(os.path.join(os.curdir, 'page_analyzer', 'database.sql')).read())
